48e68b7e82004ad0af9a95bafd1b365a7b923528
haykel88 <haikel.ouhichi@gmail.com>
2014-12-25 21:28:50 +0100

    first commit

---
+++ MotorControlBean/MotorControlBean/MyMotorControlBean.cs
@@ -0,0 +1,319 @@
+﻿/*
+ * Crée par SharpDevelop.
+ * Utilisateur: OUHICHI
+ * Date: 25/12/2014
+ * Heure: 19:21
+ * 
+ * Pour changer ce modèle utiliser Outils | Options | Codage | Editer les en-têtes standards.
+ */
+using System;
+using System.Text;
+using WComp.Beans;
+using Phidgets; //Needed for the MotorControl class, Phidget base classes, and the PhidgetException class
+using Phidgets.Events; //Needed for the Phidget event handling classes
+
+namespace WComp.Beans
+{
+	/// <summary>
+	/// This is a sample bean, which has an integer evented property and a method.
+	/// 
+	/// Notes: for beans creating threads, the IThreadCreator interface should be implemented,
+	/// 	providing a cleanup method should be implemented and named `Stop()'.
+	/// For proxy beans, the IProxyBean interface should  be implemented,
+	/// 	providing the IsConnected property, allowing the connection status to be drawn in
+	/// 	the AddIn's graphical designer.
+	/// 
+	/// Several classes can be defined or used by a Bean, but only the class with the
+	/// [Bean] attribute will be available in WComp. Its ports will be all public methods,
+	/// events and properties definied in that class.
+	/// </summary>
+	[Bean(Category="MyCategory")]
+	public class MyMotorControlBean
+	{
+		/// <summary>
+		/// Fill in private attributes here.
+		/// </summary>
+		private MotorControl motoControl; //Declare a MotorControl object
+		
+		
+		//initialize the MotorConrol object and hook the event handlers
+        public void loadMotorControl()
+        {
+            motoControl = new MotorControl();
+
+            motoControl.Attach += new AttachEventHandler(motoControl_Attach);
+            motoControl.Detach += new DetachEventHandler(motoControl_Detach);
+            motoControl.Error += new ErrorEventHandler(motoControl_Error);
+
+            motoControl.CurrentChange += new CurrentChangeEventHandler(motoControl_CurrentChange);
+            motoControl.InputChange += new InputChangeEventHandler(motoControl_InputChange);
+            motoControl.VelocityChange += new VelocityChangeEventHandler(motoControl_VelocityChange);
+            motoControl.BackEMFUpdate += new BackEMFUpdateEventHandler(motoControl_BackEMFUpdate);
+            motoControl.EncoderPositionChange += new EncoderPositionChangeEventHandler(motoControl_EncoderPositionChange);
+            motoControl.SensorUpdate += new SensorUpdateEventHandler(motoControl_SensorUpdate);
+
+            openCmdLine(motoControl);
+        }
+		
+		
+		
+		//When the form is being close, make sure to stop all the motors and close the Phidget.
+        public void stopMotorControl()
+        {
+            motoControl.Attach -= motoControl_Attach;
+            motoControl.Detach -= motoControl_Detach;
+            motoControl.Error -= motoControl_Error;
+
+            motoControl.InputChange -= motoControl_InputChange;
+            motoControl.CurrentChange -= motoControl_CurrentChange;
+            motoControl.VelocityChange -= motoControl_VelocityChange;
+            motoControl.BackEMFUpdate -= motoControl_BackEMFUpdate;
+            motoControl.EncoderPositionChange -= motoControl_EncoderPositionChange;
+            motoControl.SensorUpdate -= motoControl_SensorUpdate;
+
+            if (motoControl.Attached)
+            {
+                foreach (MotorControlMotor motor in motoControl.motors)
+                {
+                    motor.Velocity = 0;
+                }
+            }
+
+            //run any events in the message queue - otherwise close will hang if there are any outstanding events
+            //Application.DoEvents();
+
+            motoControl.close();
+
+            motoControl = null;
+        }
+		
+		
+        
+        
+        void motoControl_Error(object sender, ErrorEventArgs e)
+        {
+            
+        }
+         void motoControl_CurrentChange(object sender, CurrentChangeEventArgs e)
+        {
+            
+        }
+          void motoControl_InputChange(object sender, InputChangeEventArgs e)
+        {
+            
+        }
+        void motoControl_VelocityChange(object sender, VelocityChangeEventArgs e)
+        {
+            
+        }
+        void motoControl_BackEMFUpdate(object sender, BackEMFUpdateEventArgs e)
+        {
+            
+        }
+        void motoControl_EncoderPositionChange(object sender, EncoderPositionChangeEventArgs e)
+        {
+            
+        }
+        void motoControl_SensorUpdate(object sender, SensorUpdateEventArgs e)
+        {
+            
+        }
+
+		
+		
+		
+        //MotorControl Attach event handler...populate the fields and controls
+        void motoControl_Attach(object sender, AttachEventArgs e)
+        {
+            MotorControl attached = (MotorControl)sender;
+           
+            //supplyVoltageTimer.Start();
+            
+        }
+        
+  
+        void motoControl_Detach(object sender, DetachEventArgs e)
+        {
+        	 MotorControl detached = (MotorControl)sender;
+           
+            //supplyVoltageTimer.Stop();
+        	
+        }
+        
+        
+
+
+
+        //When the acceleration trakbar is changed send this data to the motor control to affect the motor
+        //If no motor control is attached, a PhidgetException will be thrown, so we will catch it and deal
+        //with it accordingly
+        public void setAcceleration(int acceleration)
+        {
+            try
+            {
+                motoControl.motors[0].Acceleration = acceleration;
+            }
+            catch { }
+        }
+
+        //When the velocity trakbar is changed send this data tothe motor control to affect the motor
+        //If no motor control is attached, a PhidgetException will be thrown, so we will catch it and deal
+        //with it accordingly
+        public void setVelocity(int velocity)
+        {
+            try
+            {
+                motoControl.motors[0].Velocity = velocity;
+            }
+            catch { }
+        }
+
+        public void setBraking(int braking)
+        {
+            try
+            {
+                motoControl.motors[0].Braking = braking;
+            }
+            catch { }
+        }    
+
+        public void setBackEmfSensing(Boolean backEmfSensing)
+        {
+            try
+            {
+                motoControl.motors[0].BackEMFSensing = backEmfSensing;
+            }
+            catch { }
+        }
+
+        public void resetEncoder()
+        {
+            try
+            {
+                motoControl.encoders[0].Position = 0;
+            }
+            catch { }
+        }
+
+        public void setRatiometric(Boolean ratiometric)
+        {
+            try
+            {
+                motoControl.Ratiometric = ratiometric;
+            }
+            catch { }
+        }
+
+        //Parses command line arguments and calls the appropriate open
+        #region Command line open functions
+        private void openCmdLine(Phidget p)
+        {
+            openCmdLine(p, null);
+        }
+        private void openCmdLine(Phidget p, String pass)
+        {
+            int serial = -1;
+            String logFile = null;
+            int port = 5001;
+            String host = null;
+            bool remote = false, remoteIP = false;
+            string[] args = Environment.GetCommandLineArgs();
+            String appName = args[0];
+
+            try
+            { //Parse the flags
+                for (int i = 1; i < args.Length; i++)
+                {
+                    if (args[i].StartsWith("-"))
+                        switch (args[i].Remove(0, 1).ToLower())
+                        {
+                            case "l":
+                                logFile = (args[++i]);
+                                break;
+                            case "n":
+                                serial = int.Parse(args[++i]);
+                                break;
+                            case "r":
+                                remote = true;
+                                break;
+                            case "s":
+                                remote = true;
+                                host = args[++i];
+                                break;
+                            case "p":
+                                pass = args[++i];
+                                break;
+                            case "i":
+                                remoteIP = true;
+                                host = args[++i];
+                                if (host.Contains(":"))
+                                {
+                                    port = int.Parse(host.Split(':')[1]);
+                                    host = host.Split(':')[0];
+                                }
+                                break;
+                            default:
+                                goto usage;
+                        }
+                    else
+                        goto usage;
+                }
+                if (logFile != null)
+                    Phidget.enableLogging(Phidget.LogLevel.PHIDGET_LOG_INFO, logFile);
+                if (remoteIP)
+                    p.open(serial, host, port, pass);
+                else if (remote)
+                    p.open(serial, host, pass);
+                else
+                    p.open(serial);
+                return; //success
+            }
+            catch { }
+        usage:
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Invalid Command line arguments." + Environment.NewLine);
+            sb.AppendLine("Usage: " + appName + " [Flags...]");
+            sb.AppendLine("Flags:\t-n   serialNumber\tSerial Number, omit for any serial");
+            sb.AppendLine("\t-l   logFile\tEnable phidget21 logging to logFile.");
+            sb.AppendLine("\t-r\t\tOpen remotely");
+            sb.AppendLine("\t-s   serverID\tServer ID, omit for any server");
+            sb.AppendLine("\t-i   ipAddress:port\tIp Address and Port. Port is optional, defaults to 5001");
+            sb.AppendLine("\t-p   password\tPassword, omit for no password" + Environment.NewLine);
+            sb.AppendLine("Examples: ");
+            sb.AppendLine(appName + " -n 50098");
+            sb.AppendLine(appName + " -r");
+            sb.AppendLine(appName + " -s myphidgetserver");
+            sb.AppendLine(appName + " -n 45670 -i 127.0.0.1:5001 -p paswrd");
+          //  MessageBox.Show(sb.ToString(), "Argument Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+          //  Application.Exit();
+        }
+        #endregion
+    
+	
+	
+	
+	
+	   
+		/// <summary>
+		/// Here are the delegate and his event.
+		/// A function checking nullity should be used to fire events (like FireIntEvent).
+		/// </summary>
+		public delegate void IntValueEventHandler(int val);
+		/// <summary>
+		/// the following declaration is the event by itself. Its name, here "PropertyChanged",
+		/// is the name of the event as it will be displayed in the bean type's interface.
+		/// </summary>
+		public event IntValueEventHandler PropertyChanged;
+		
+		private void FireIntEvent(int i) {
+			if (PropertyChanged != null)
+				PropertyChanged(i);
+		}	
+		
+		
+		
+	
+		
+	}
+}
---
+++ Tests/TestMyMotorControl.cs
@@ -0,0 +1,125 @@
+﻿/*
+ * Crée par SharpDevelop.
+ * Utilisateur: OUHICHI
+ * Date: 25/12/2014
+ * Heure: 20:37
+ * 
+ * Pour changer ce modèle utiliser Outils | Options | Codage | Editer les en-têtes standards.
+ */
+
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+using WComp.Beans;
+
+namespace DefaultNamespace
+{
+	/// <summary>
+	/// Description of Container1.
+	/// </summary>
+	public class Container1 : System.Windows.Forms.Form
+	{
+        [BeanDesignLocation(592,192)]
+        private WComp.Beans.MyMotorControlBean myMotorControlBean1;
+        [BeanDesignLocation(176,72)]
+        private System.Windows.Forms.Button button1;
+        [BeanDesignLocation(176,152)]
+        private System.Windows.Forms.Button button2;
+        [BeanDesignLocation(160,320)]
+        private System.Windows.Forms.TrackBar trackBar1;
+		public Container1()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+
+			//
+			// The InitializeBeans() call is required for WComp.NET designer support.
+			//
+			InitializeBeans();
+			
+			//
+			// TODO: Add constructor code after the InitializeBeans() call.
+			//
+		}
+		
+		[STAThread]
+		public static void Main(string[] args)
+		{
+			Application.Run(new Container1());
+		}
+		
+		#region WComp.NET designer generated code
+		/// <summary>
+		/// This method is required for WComp.NET designer support.
+		/// Do not change the method contents inside the source code editor.
+		/// The WComp.NET designer might not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent() {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            // 
+            // button1
+            // 
+            this.button1.Text = "Start";
+            this.button1.Controls = null;
+            this.button1.DataBindings = null;
+            this.button1.Location = new System.Drawing.Point(176, 72);
+            // 
+            // button2
+            // 
+            this.button2.Text = "Stop";
+            this.button2.Controls = null;
+            this.button2.DataBindings = null;
+            this.button2.Location = new System.Drawing.Point(176, 152);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Minimum = -100;
+            this.trackBar1.Controls = null;
+            this.trackBar1.DataBindings = null;
+            this.trackBar1.Location = new System.Drawing.Point(160, 320);
+            // 
+            // TestMyMotorControl
+            // 
+            this.Text = "SharpWComp static application";
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.trackBar1);
+        }
+		
+		/// <summary>
+		/// This method is required for WComp.NET designer support.
+		/// Do not change the method contents inside the source code editor.
+		/// The WComp.NET designer might not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeBeans() {
+            this.myMotorControlBean1 = new WComp.Beans.MyMotorControlBean();
+            // 
+            Control.CheckForIllegalCrossThreadCalls = false;
+            // 
+            // Event dispatching
+            // 
+            this.button1.Click += new System.EventHandler(this.@__button1_to_myMotorControlBean1_0);
+            this.button2.Click += new System.EventHandler(this.@__button2_to_myMotorControlBean1_1);
+            this.trackBar1.ValueChanged += new System.EventHandler(this.@__trackBar1_to_myMotorControlBean1_2);
+        }
+
+		private void @__button1_to_myMotorControlBean1_0(object sender, System.EventArgs e) {
+            this.myMotorControlBean1.loadMotorControl();
+        }
+
+		private void @__button2_to_myMotorControlBean1_1(object sender, System.EventArgs e) {
+            this.myMotorControlBean1.stopMotorControl();
+        }
+
+		private void @__trackBar1_to_myMotorControlBean1_2(object sender, System.EventArgs e) {
+            this.myMotorControlBean1.setVelocity(this.trackBar1.Value);
+        }
+		#endregion
+	}
+}
